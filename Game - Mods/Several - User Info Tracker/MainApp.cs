46e5c95012092f6f81afc19ec59553947df879ef
Nixy <93561149+NixyDev@users.noreply.github.com>
2022-07-10 18:54:14 -0500

    Add files via upload

---
+++ Game - Mods/Several - User Info Tracker/MainApp.cs
@@ -0,0 +1,3 @@
+if game.PlaceId == 286090429 then
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/NixyDev/Nx-Store/main/Game%20-%20Mods/Several%20-%20User%20Info%20Tracker/Patch%20-%20Arsenal.lua'))()
+end
